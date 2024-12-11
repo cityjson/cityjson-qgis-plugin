080f0a5014d24112bdfc0c99828bf43e114596ea
Frédéric Leroux <leroux_frederic33@hotmail.com>
2024-12-11 09:43:28 -0500

    Remove blanks

--- core/geometry.py
+++ core/geometry.py
@@ -9,6 +9,7 @@ class VerticesCache:
         self._scale = scale
         self._translate = translate
         self._vertices = []
+
         if vertices is not None:
             for vertex in vertices:
                 self.add_vertex(vertex)
@@ -39,9 +40,7 @@ class VerticesCache:
         return self._vertices[index]
 
 class TransformedVerticesCache:
-    """A class that decorates a VerticesCache applying a decoration when
-    vertices are requested.
-    """
+    """A class that decorates a VerticesCache applying a decoration when vertices are requested"""
 
     def __init__(self, decorated, translation, transformation_matrix=None):
         """Initiates the transformed vertices cache with the provided
@@ -54,6 +53,7 @@ class TransformedVerticesCache:
         """
         self._decorated = decorated
         self._translation = translation
+
         if transformation_matrix is None:
             self._transformation_matrix = [1.0, 0.0, 0.0, 0.0,
                                            0.0, 1.0, 0.0, 0.0,
@@ -68,6 +68,7 @@ class TransformedVerticesCache:
         x = original_vertex.x() + self._translation.x()
         y = original_vertex.y() + self._translation.y()
         z = original_vertex.z() + self._translation.z()
+
         return QgsPoint(x, y, z)
 
 class GeometryReader:
@@ -88,16 +89,16 @@ class GeometryReader:
             self._templates_vertices_cache = template_vertex_cache
 
     def read_geometry(self, geometry, lod="All"):
-        """Reads a CityJSON geometry and returns it as QgsGeometry
-        """
+        """Reads a CityJSON geometry and returns it as QgsGeometry"""
         polygons, _ = self.get_polygons(geometry, lod)
 
         return self.polygons_to_geometry(polygons)
 
     def has_lod(self, geometries, target_lod):
-        """Checks if any geometry in the list matches the specified LoD."""
+        """Checks if any geometry in the list matches the specified LoD"""
         if target_lod == "All":
             return True
+
         return any(self.get_lod(geom) == target_lod for geom in geometries)
 
     def get_lod(self, geometry):
@@ -114,17 +115,15 @@ class GeometryReader:
         for polygon in polygons:
             g = self.read_polygon(polygon)
             geoms.addGeometry(g)
+
         return QgsGeometry(geoms)
 
     def get_polygons(self, geometry, attributes={}):
-        """Returns a dictionary where keys are polygons and values
-        are the semantic surfaces
-        """
+        """Returns a dictionary where keys are polygons and values are the semantic surfaces"""
         polygons = []
         semantics = []
 
         for geom in geometry:
-            # Skip geometries that don't match the target LoD
             if not self.has_lod([geom], self.lod) or not self.get_lod(geom):
                 continue
 
@@ -137,14 +136,13 @@ class GeometryReader:
                 temp_geom = geom
                 temp_vertices_cache = self._vertices_cache
 
-            # Prepare a dictionary to hold surface attributes
             additional_semantics = {}
             try:
                 if "semantics" in temp_geom:
                     surfaces = temp_geom["semantics"]["surfaces"]
                     values = temp_geom["semantics"]["values"]
+
                     if len(attributes) > 2:
-                        # Collect additional attributes
                         for attr in attributes:
                             attr = '+' + attr
                             if attr.lstrip("+") not in ['type','on_footprint_edge'] and attr in temp_geom["semantics"]:
@@ -155,7 +153,6 @@ class GeometryReader:
 
                 new_polygons, new_semantics = read_boundaries(temp_geom["boundaries"], surfaces, values)
                 new_polygons = self.indexes_to_points(new_polygons, temp_vertices_cache)
-
                 polygons += new_polygons
 
                 if len(additional_semantics) > 0:
