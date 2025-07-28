31c81d8e45e0dc54d3013470e5c0159c0cfb221e
GinaStavropoulou <ginastavropoulou@gmail.com>
2025-07-28 17:45:36 +0200

    fixed test_layers

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -2,6 +2,7 @@ import pytest
 
 from core.layers import TypeNamingIterator, BaseFieldsBuilder, NullFieldsBuilder, AttributeFieldsDecorator, LodFieldsDecorator, SemanticSurfaceFieldsDecorator
 
+SINGLE_CUBE_CITYMODEL = {"CityObjects": {"id-1": {"geometry": [{"boundaries": [[[[0,1,2,3]],[[4,5,1,0]],[[5,6,2,1]],[[6,7,3,2]],[[7,4,0,3]],[[7,6,5,4]]]],"lod": "1","type": "Solid", "semantics": {"surfaces": [{"type": "RoofSurface","material": "tile"},{"type": "WallSurface","material": "brick"},{"type": "WallSurface","material": "brick"},{"type": "WallSurface","material": "brick"},{"type": "WallSurface","material": "brick"},{"type": "GroundSurface","material": "concrete"}],"values": [0, 1, 2, 3, 4, 5]}}],"attributes": {"function": "something"},"type": "GenericCityObject"}},"type": "CityJSON","version": "2.0","vertices": [[0,0,1000],[1000,0,1000],[1000,1000,1000],[0,1000,1000],[0,0,0],[1000,0,0],[1000,1000,0],[0,1000,0]],"metadata": {"geographicalExtent": [0.0,0.0,0.0,1.0,1.0,1.0]},"transform": {"scale": [0.001,0.001,0.001],"translate": [0.0,0.0,0.0]},"extensions": {}}
 two_cubes_citymodel = {"CityObjects":{"id-1":{"geometry":[{"boundaries":[[[0,1,2,3]],[[7,4,0,3]],[[4,5,1,0]],[[5,6,2,1]],[[3,2,6,7]],[[6,5,4,7]]],"lod":1,"type":"MultiSurface"}],"type":"GenericCityObject"}},"type":"CityJSON","version":"0.9","vertices":[[1.0,0.0,1.0],[0.0,1.0,1.0],[-1.0,0.0,1.0],[0.0,-1.0,1.0],[1.0,0.0,0.0],[0.0,1.0,0.0],[-1.0,0.0,0.0],[0.0,-1.0,0.0]],"metadata":{"geographicalExtent":[-1.0,-1.0,0.0,1.0,1.0,1.0]}}
 citymodel_with_attributes = {"type":"CityJSON","version":"0.9","CityObjects":{"id-1":{"type":"Building","attributes":{"attribute1":1,"attribute2":2}},"id-2":{"type":"Building","attributes":{"attribute1":1,"attribute3":2}}}}
 
@@ -27,9 +28,11 @@ class TestFieldBuilders:
 
         fields = builder.get_fields()
 
-        assert len(fields) == 2
+        assert len(fields) == 4
         assert fields[0].name() == "uid"
         assert fields[1].name() == "type"
+        assert fields[2].name() == "parents"
+        assert fields[3].name() == "children"
     
     def test_attributes_fields_builder(self):
         """Tests that AttributeFieldsDecorator creates the attributes of the model"""
@@ -57,9 +60,10 @@ class TestFieldBuilders:
     def test_semantic_surface_fields_builder(self):
         """Tests that LodFieldsBuilder create the lod field"""
         builder = NullFieldsBuilder()
-        builder = SemanticSurfaceFieldsDecorator(builder)
+        builder = SemanticSurfaceFieldsDecorator(builder, SINGLE_CUBE_CITYMODEL)
 
         fields = builder.get_fields()
 
-        assert len(fields) == 1
-        assert fields[0].name() == "semantic_surface"
+        assert len(fields) == 2
+        assert fields[0].name() == "surface.type"
+        assert fields[1].name() == "surface.material"
